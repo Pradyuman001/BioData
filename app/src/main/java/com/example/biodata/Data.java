727e0ef72cb812f746d7ea373c5d7d64ed03fb3f
Pradyuman <pradyumangodhani006@gmail.com>
2022-03-22 07:30:46 +0530

    21-03-2022

--- app/src/main/java/com/example/biodata/Data.java
+++ app/src/main/java/com/example/biodata/Data.java
@@ -6,13 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Data extends AppCompatActivity {
 
-    private EditText surname_edt,name_edt,fname_edt,mothername_edt,bsname_edt,dob_edt,birth_edt,edu_edt;
+    private EditText fullname_edt,dob_edt,birth_edt,edu_edt,height_edt,num_edt,blood_edt,fname_edt,occ_edt,add_edt,fnum_edt,mothername_edt,brothername_edt,sistername_edt,village_edt,taluka_edt,district_edt,email_edt;
     Button submit_btn;
+    RadioGroup radio_group;
+    RadioButton male_rd_btn,female_rd_btn;
+    CheckBox read_chk,music_chk,dance_chk;
+    String gender,reading,music,dancing;
+    SeekBar age_seek;
+    TextView age1_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,74 +33,232 @@ public class Data extends AppCompatActivity {
 
         binding();
 
+        read_chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if(b==true){
+
+                    reading="Reading";
+                }
+
+                else {
+
+                    reading = " ";
+
+                }
+            }
+        });
+
+        music_chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if(b==true){
+
+                    music = "Music";
+
+                }
+
+                else {
+
+                    music = " ";
+                }
+            }
+        });
+
+        dance_chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if(b==true){
+
+                    dancing = "Dancing";
+                }
+
+                else {
+
+                    dancing = " ";
+                }
+            }
+        });
+
+        age_seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                age1_txt.setText(""+i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         submit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(surname_edt.getText().toString().length()<=0){
-                    surname_edt.setError("Enter Surname");
-                }
-                else if(name_edt.getText().toString().length()<=0){
-                    name_edt.setError("Enter Name");
-                }
-                else if(fname_edt.getText().toString().length()<=0){
-                    fname_edt.setError("Enter Father Name");
-                }
-                else if(mothername_edt.getText().toString().length()<=0){
-                    mothername_edt.setError("Enter Mother Name");
-                }
-                else if(bsname_edt.getText().toString().length()<=0){
-                    bsname_edt.setError("Enter Brother/Sister Name");
+                if(fullname_edt.getText().toString().length()<=0){
+                    fullname_edt.setError("Enter Full Name");
                 }
                 else if(dob_edt.getText().toString().length()<=0){
-                    dob_edt.setError("Enter Date Of Birth Name");
+                    dob_edt.setError("Enter Date Of Birth");
                 }
                 else if(birth_edt.getText().toString().length()<=0){
-                    birth_edt.setError("Enter Place Of Birth Name");
+                    birth_edt.setError("Enter Place Of Birth");
                 }
                 else if(edu_edt.getText().toString().length()<=0){
                     edu_edt.setError("Enter Education");
                 }
+                else if(email_edt.getText().toString().length()<=0){
+                    email_edt.setError("Enter Email Address");
+                }
+                else if(height_edt.getText().toString().length()<=0){
+                    height_edt.setError("Enter Height");
+                }
+                else if(num_edt.getText().toString().length()<=0){
+                    num_edt.setError("Enter Mobile Number");
+                }
+                else if(blood_edt.getText().toString().length()<=0){
+                    blood_edt.setError("Enter Blood Group");
+                }
+
+                else if(age1_txt.getText().toString().length()==0){
+                    age1_txt.setError("Enter Age");
+                }
+                else if(fname_edt.getText().toString().length()<=0){
+                    fname_edt.setError("Enter Father Name");
+                }
+                else if(occ_edt.getText().toString().length()<=0){
+                    occ_edt.setError("Enter Father's Occupation");
+                }
+                else if(add_edt.getText().toString().length()<=0){
+                    add_edt.setError("Enter Address");
+                }
+                else if(fnum_edt.getText().toString().length()<=0){
+                    fnum_edt.setError("Enter Father's Mobile No.");
+                }
+                else if(mothername_edt.getText().toString().length()<=0){
+                    mothername_edt.setError("Enter Mother Name");
+                }
+                else if(village_edt.getText().toString().length()<=0){
+                    village_edt.setError("Enter Village Name");
+                }
+                else if(taluka_edt.getText().toString().length()<=0){
+                    taluka_edt.setError("Enter Taluka Name");
+                }
+                else if(district_edt.getText().toString().length()<=0){
+                    district_edt.setError("Enter District Name");
+                }
+
 
 
                 else{
-                    String surname = surname_edt.getText().toString();
-                    String name = name_edt.getText().toString();
-                    String fname = fname_edt.getText().toString();
-                    String mother = mothername_edt.getText().toString();
-                    String bsname = bsname_edt.getText().toString();
+                    String fullname = fullname_edt.getText().toString();
                     String dob = dob_edt.getText().toString();
                     String birth = birth_edt.getText().toString();
                     String edu = edu_edt.getText().toString();
+                    String height = height_edt.getText().toString();
+                    String num = num_edt.getText().toString();
+                    String blood = blood_edt.getText().toString();
+                    String fname = fname_edt.getText().toString();
+                    String occ = occ_edt.getText().toString();
+                    String add = add_edt.getText().toString();
+                    String fnum = fnum_edt.getText().toString();
+                    String mothername = mothername_edt.getText().toString();
+                    String brothername = brothername_edt.getText().toString();
+                    String sistername = sistername_edt.getText().toString();
+                    String village = village_edt.getText().toString();
+                    String taluka = taluka_edt.getText().toString();
+                    String district = district_edt.getText().toString();
+                    String age = age1_txt.getText().toString();
+                    String email = email_edt.getText().toString();
 
+                    if(radio_group.getCheckedRadioButtonId() == R.id.male_rd_btn){
+                        gender = "Male";
+                    }
+                    else if(radio_group.getCheckedRadioButtonId() == R.id.female_rd_btn){
+                        gender = "Female";
+                    }
 
                     //Toast.makeText(Data.this, surname+"\n"+name+"\n"+fname+"\n"+mother, Toast.LENGTH_SHORT).show();
 
                     Intent intent = new Intent(Data.this, MainActivity.class);
-                    intent.putExtra("n1", surname);
-                    intent.putExtra("n2", name);
-                    intent.putExtra("n3", fname);
-                    intent.putExtra("n4", mother);
-                    intent.putExtra("n5", bsname);
-                    intent.putExtra("n6", dob);
-                    intent.putExtra("n7", birth);
-                    intent.putExtra("n8", edu);
+
+                    intent.putExtra("n1", fullname);
+                    intent.putExtra("n2", dob);
+                    intent.putExtra("n3", birth);
+                    intent.putExtra("n4", edu);
+                    intent.putExtra("n5", height);
+                    intent.putExtra("n6", num);
+                    intent.putExtra("n7", blood);
+                    intent.putExtra("n8", gender);
+                    intent.putExtra("n10", fname);
+                    intent.putExtra("n11", occ);
+                    intent.putExtra("n12", add);
+                    intent.putExtra("n13", fnum);
+                    intent.putExtra("n14", mothername);
+                    intent.putExtra("n15", brothername);
+                    intent.putExtra("n16", sistername);
+                    intent.putExtra("n17", village);
+                    intent.putExtra("n18", taluka);
+                    intent.putExtra("n19", district);
+                    intent.putExtra("n20", reading);
+                    intent.putExtra("n21", music);
+                    intent.putExtra("n22", dancing);
+                    intent.putExtra("n23", age);
+                    intent.putExtra("n25", email);
+
+
                     startActivity(intent);
                 }
+
+
             }
         });
     }
 
     private void binding() {
 
-        surname_edt = findViewById(R.id.surname_edt);
-        name_edt = findViewById(R.id.name_edt);
-        fname_edt = findViewById(R.id.fname_edt);
-        mothername_edt = findViewById(R.id.mothername_edt);
-        bsname_edt = findViewById(R.id.bsname_edt);
+        fullname_edt = findViewById(R.id.fullname_edt);
         dob_edt = findViewById(R.id.dob_edt);
+        fname_edt = findViewById(R.id.fname_edt);
         birth_edt = findViewById(R.id.birth_edt);
         edu_edt = findViewById(R.id.edu_edt);
+        height_edt = findViewById(R.id.height_edt);
+        num_edt = findViewById(R.id.num_edt);
+        blood_edt = findViewById(R.id.blood_edt);
+        occ_edt = findViewById(R.id.occ_edt);
+        add_edt = findViewById(R.id.add_edt);
+        fnum_edt = findViewById(R.id.fnum_edt);
+        mothername_edt = findViewById(R.id.mothername_edt);
+        brothername_edt = findViewById(R.id.brothername_edt);
+        sistername_edt = findViewById(R.id.sistername_edt);
+        village_edt = findViewById(R.id.village_edt);
+        taluka_edt = findViewById(R.id.taluka_edt);
+        district_edt = findViewById(R.id.district_edt);
+        email_edt = findViewById(R.id.email_edt);
+
+
+        age1_txt = findViewById(R.id.age1_txt);
+
+
+
+        radio_group = findViewById(R.id.radio_group);
+        male_rd_btn = findViewById(R.id.male_rd_btn);
+        female_rd_btn = findViewById(R.id.female_rd_btn);
+        read_chk = findViewById(R.id.read_chk);
+        music_chk = findViewById(R.id.music_chk);
+        dance_chk = findViewById(R.id.dance_chk);
         submit_btn = findViewById(R.id.submit_btn);
+        submit_btn = findViewById(R.id.submit_btn);
+        age_seek = findViewById(R.id.age_seek);
+
     }
 }
--- app/src/main/java/com/example/biodata/MainActivity.java
+++ app/src/main/java/com/example/biodata/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.biodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,7 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    private TextView surname_txt,name_txt,fname_txt,mothername_txt,bsname_txt,dob_txt,birth_txt,edu_txt;
+    private TextView fullname_txt,dob_txt,birth_txt,edu_txt,height_txt,num_txt,blood_txt,gender_txt,fname_txt,occ_txt,add_txt,fnum_txt,mothername_txt,brothername_txt,sistername_txt,village_txt,taluka_txt,district_txt,h1_txt,h2_txt,h3_txt,age_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,33 +20,98 @@ public class MainActivity extends AppCompatActivity {
 
         binding();
 
-        String surname = getIntent().getStringExtra("n1");
-        String name = getIntent().getStringExtra("n2");
-        String fname = getIntent().getStringExtra("n3");
-        String mothername = getIntent().getStringExtra("n4");
-        String bsname = getIntent().getStringExtra("n5");
-        String dob = getIntent().getStringExtra("n6");
-        String birth = getIntent().getStringExtra("n7");
-        String edu = getIntent().getStringExtra("n8");
+        String fullname = getIntent().getStringExtra("n1");
+        String dob = getIntent().getStringExtra("n2");
+        String birth = getIntent().getStringExtra("n3");
+        String edu = getIntent().getStringExtra("n4");
+        String height = getIntent().getStringExtra("n5");
+        String num = getIntent().getStringExtra("n6");
+        String blood = getIntent().getStringExtra("n7");
+        String gender = getIntent().getStringExtra("n8");
+        String fname = getIntent().getStringExtra("n10");
+        String occ = getIntent().getStringExtra("n11");
+        String add = getIntent().getStringExtra("n12");
+        String fnum = getIntent().getStringExtra("n13");
+        String mothername = getIntent().getStringExtra("n14");
+        String brothername = getIntent().getStringExtra("n15");
+        String sistername = getIntent().getStringExtra("n16");
+        String village = getIntent().getStringExtra("n17");
+        String taluka = getIntent().getStringExtra("n18");
+        String district = getIntent().getStringExtra("n19");
+        String h1 = getIntent().getStringExtra("n20");
+        String h2 = getIntent().getStringExtra("n21");
+        String h3 = getIntent().getStringExtra("n22");
+        String age = getIntent().getStringExtra("n23");
 
-        surname_txt.setText(surname);
-        name_txt.setText(name);
-        fname_txt.setText(fname);
-        mothername_txt.setText(mothername);
-        bsname_txt.setText(bsname);
+
+        fullname_txt.setText(fullname);
         dob_txt.setText(dob);
+        fname_txt.setText(fname);
         birth_txt.setText(birth);
         edu_txt.setText(edu);
+        gender_txt.setText(gender);
+        height_txt.setText(height);
+        occ_txt.setText(occ);
+        num_txt.setText(num);
+        blood_txt.setText(blood);
+        add_txt.setText(add);
+        fnum_txt.setText(fnum);
+        mothername_txt.setText(mothername);
+        brothername_txt.setText(brothername);
+        sistername_txt.setText(sistername);
+        village_txt.setText(village);
+        taluka_txt.setText(taluka);
+        district_txt.setText(district);
+        h1_txt.setText(h1);
+        h2_txt.setText(h2);
+        h3_txt.setText(h3);
+        age_txt.setText(age);
+
+
+        num_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String no = "tel:"+num;
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse(no));
+                startActivity(i);
+            }
+        });
+
+        fnum_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String no = "tel:"+fnum;
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse(no));
+                startActivity(i);
+            }
+        });
+
     }
 
     private void binding() {
-        surname_txt = findViewById(R.id.surname_txt);
-        name_txt = findViewById(R.id.name_txt);
-        fname_txt = findViewById(R.id.fname_txt);
-        mothername_txt = findViewById(R.id.mothername_txt);
-        bsname_txt = findViewById(R.id.bsname_txt);
+        fullname_txt = findViewById(R.id.fullname_txt);
         dob_txt = findViewById(R.id.dob_txt);
+        fname_txt = findViewById(R.id.fname_txt);
         birth_txt = findViewById(R.id.birth_txt);
         edu_txt = findViewById(R.id.edu_txt);
+        gender_txt = findViewById(R.id.gender_txt);
+        h1_txt = findViewById(R.id.h1_txt);
+        h2_txt = findViewById(R.id.h2_txt);
+        h3_txt = findViewById(R.id.h3_txt);
+        age_txt = findViewById(R.id.age_txt);
+        height_txt = findViewById(R.id.height_txt);
+        occ_txt = findViewById(R.id.occ_txt);
+        num_txt = findViewById(R.id.num_txt);
+        blood_txt = findViewById(R.id.blood_txt);
+        add_txt = findViewById(R.id.add_txt);
+        fnum_txt = findViewById(R.id.fnum_txt);
+        mothername_txt = findViewById(R.id.mothername_txt);
+        brothername_txt = findViewById(R.id.brothername_txt);
+        sistername_txt = findViewById(R.id.sistername_txt);
+        village_txt = findViewById(R.id.village_txt);
+        taluka_txt = findViewById(R.id.taluka_txt);
+        district_txt = findViewById(R.id.district_txt);
     }
 }
